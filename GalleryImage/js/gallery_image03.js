ba55b4cc910e4b10c1fb3ef28bebff6df12c3f78
lucasWeirich <lucasweirich11@gmail.com>
2022-08-10 16:00:31 -0300

    Project Gallery Images - Finalized

--- GalleryImage/js/gallery_image03.js
+++ GalleryImage/js/gallery_image03.js
@@ -7,7 +7,7 @@ $('.style03 .gallery_image .all_images').on('click', '.image', function () {
     const alt = $(this).find('img').prop('alt');
 
     if (src) {
-        $(image_main).find('img').remove()
+        $(image_main).find('img').remove();
 
         const element = `
                 <img src="${src}" alt="${alt}">
@@ -25,14 +25,44 @@ $('.style03 .gallery_image .all_images').on('click', '.image', function () {
     const images = [];
     const src = $(this).find('img').attr('src');
 
-    if (db) {
-        db.forEach(function (img) {
-            images.push(img);
-        });
-    }
 
-    images.push(src)
+    
+    if (db) {
+        
+        db.forEach(function (img) {
+            if (!(img == src)) {
+                images.push(img);
+            }
+        });
+    };
+
+    images.push(src);
 
     localStorage.setItem('img_history', JSON.stringify(images));
-    //console.log(images);
+})
+
+// -------------------------------------------------------------------------------------
+// View history images
+
+$('.style03 #search-button').on('click', function () {
+    const db = JSON.parse(localStorage.getItem('img_history'));
+    const history = $('.style03 .history_images');
+
+    if (!db) {
+        alert("Nenhuma imagem visualizada!");
+        return;
+    }
+
+    if (!history.hasClass('active')) {
+        db.forEach(function (img) {
+            const element = `
+                    <div class="image">
+                        <a download href="${img}"><img src="${img}">
+                    </div>    
+                `
+            $(history.find('.display')).append(element);
+        })
+    }
+
+    $(history).addClass('active');
 })
